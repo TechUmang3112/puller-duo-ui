9dd22edb3fabf96298c61b7204b3e693a89880e6
TechUmang3112 <135492229+TechUmang3112@users.noreply.github.com>
2025-03-20 21:06:36 +0530

    Changes for logo

--- src/app/(DashboardLayout)/layout/shared/logo/Logo.tsx
+++ src/app/(DashboardLayout)/layout/shared/logo/Logo.tsx
@@ -1,4 +1,6 @@
 "use client";
+
+// Imports
 import { useSelector } from "@/store/hooks";
 import Link from "next/link";
 import { styled } from "@mui/material/styles";
@@ -7,6 +9,8 @@ import Image from "next/image";
 
 const Logo = () => {
   const customizer = useSelector((state: AppState) => state.customizer);
+
+  // Styled component for the Link
   const LinkStyled = styled(Link)(() => ({
     height: customizer.TopbarHeight,
     width: customizer.isCollapse ? "40px" : "180px",
@@ -14,49 +18,20 @@ const Logo = () => {
     display: "block",
   }));
 
-  if (customizer.activeDir === "ltr") {
-    return (
-      <LinkStyled href="/">
-        {customizer.activeMode === "dark" ? (
-          <Image
-            src="/images/logos/logoP.png"
-            alt="logo"
-            height={customizer.TopbarHeight}
-            width={174}
-            priority
-          />
-        ) : (
-          <Image
-            src={"/images/logos/dark-logo.svg"}
-            alt="logo"
-            height={customizer.TopbarHeight}
-            width={174}
-            priority
-          />
-        )}
-      </LinkStyled>
-    );
-  }
-
   return (
     <LinkStyled href="/">
-      {customizer.activeMode === "dark" ? (
-        <Image
-          src="/images/logos/dark-rtl-logo.svg"
-          alt="logo"
-          height={customizer.TopbarHeight}
-          width={174}
-          priority
-        />
-      ) : (
-        <Image
-          src="/images/logos/light-logo-rtl.svg"
-          alt="logo"
-          height={customizer.TopbarHeight}
-          width={174}
-          priority
-        />
-      )}
+      <Image
+        src="/images/logos/pullerduologo.svg"
+        alt="logo"
+        height={customizer.TopbarHeight} // Set height dynamically
+        width={customizer.isCollapse ? 40 : 180} // Set width dynamically
+        //height={110}
+        //width={250}
+        priority
+        style={{
+          objectFit: "contain", // Ensures the image fits within the container
+        }}
+      />
     </LinkStyled>
   );
 };
